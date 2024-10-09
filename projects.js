077e9f11c3cc207bb28c7580073df4e458d9c6d4
Q <kylenguyen1206@gmail.com>
2024-10-09 18:23:41 -0400

    update website

--- projects.js
+++ projects.js
@@ -24,12 +24,15 @@ function checkProjectGameState() {
     
 }
 
-function toggleDescription(desc_id) {
+function toggleDescription(desc_id, arrow_id) {
     const desc = document.getElementById(desc_id);
+    const arrow_icon = document.getElementById(arrow_id);
     if (desc.style.display === "block") {
         desc.style.display = "none";
+        arrow_icon.src = "side_arrow.png";
     } else {
         desc.style.display = "block";
+        arrow_icon.src = "down_arrow.png";
     }
 }
 
