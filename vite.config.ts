9aa6bee411647001865e83a8eb6d2ccca612d12c
Q <kylenguyen1206@gmail.com>
2024-11-20 00:28:42 -0500

    update 11/20/24 -add github actions

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/Kairu1206/',
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
