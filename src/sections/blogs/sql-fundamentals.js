8110519c50474e6649cecccd0f4b3d5c16d98773
Quang <kylenguyen1206@gmail.com>
2026-02-01 21:02:49 -0500

    update website

--- src/sections/blogs/sql-fundamentals.js
+++ src/sections/blogs/sql-fundamentals.js
@@ -240,6 +240,89 @@ In the list of available databases, you should also see the \`task_4_db\` databa
 Let's explore all these different operations when working with **MySQL**. In the next two tasks, we will be using the **books table** that is part of the database **thm_books**. We can access it with the statement \`use thm_books;\`.
 
 **Create Operation (INSERT)**
+The **Create** operation will create new records in a table. In MySQL, this can be achieved by using the statement \`INSERT INTO\`, as shown below.
+\`\`\`
+mysql> INSERT INTO books (id, name, published_date, description)
+    VALUES (1, "Android Security Internals", "2014-10-14", "An In-Depth Guide to Android's Security Architecture");
+
+Query OK, 1 row affected (0.01 sec)
+\`\`\`
+
+As we can observe, the \`INSERT INTO\` statement specifies a table, in this case, **books**, where you can add a new record; the columns **id**, **name**, **published_date**, and **description** are the records in the table. In this example, a new record with an **id** of **1**, a **name** of **"Android Security Internals"**, a **published_date** of **"2014-10-14"**, and a **description** stating **"Android Security Internals provides a complete understanding of the security internals of Android devices"** was added.
+
+**Note**: This operation already exists in the database so there is no need to run the query.
+
+**Read Operation (SELECT)**
+The **Read** operation, as the name suggests, is used to read or retrieve information from a table. We can fetch a column or all columns from a table with the \`SELECT\` statement, as shown in the next example.
+\`\`\`
+mysql> SELECT * FROM books;
++----+----------------------------+----------------+------------------------------------------------------+
+| id | name                       | published_date | description                                          |
++----+----------------------------+----------------+------------------------------------------------------+
+|  1 | Android Security Internals | 2014-10-14     | An In-Depth Guide to Android's Security Architecture |
++----+----------------------------+----------------+------------------------------------------------------+
+
+1 row in set (0.00 sec)
+\`\`\`
+
+The above output \`SELECT\` statement is followed by an \`*\` symbol indicating that all columns should be retrieved, followed by the \`FROM\` clause and the table name, in this case, **books**.
+
+If we want to select a specific column like the **name** and **description**, we should specify them instead of the \`*\` symbol, as shown below.
+\`\`\`
+mysql> SELECT name, description FROM books;
++----------------------------+------------------------------------------------------+
+| name                       | description                                          |
++----------------------------+------------------------------------------------------+
+| Android Security Internals | An In-Depth Guide to Android's Security Architecture |
++----------------------------+------------------------------------------------------+
+
+1 row in set (0.00 sec)
+\`\`\`
+
+**Update Operation (UPDATE)**
+The **Update** operation modifies an existing record within a table, and the same statement, \`UPDATE\`, can be used for this.
+\`\`\`
+mysql> UPDATE books
+    SET description = "An In-Depth Guide to Android's Security Architecture."
+    WHERE id = 1;
+
+Query OK, 1 row affected (0.00 sec)
+Rows matched: 1  Changed: 1  Warnings: 0
+\`\`\`
+
+The \`UPDATE\` statement specifies the table, in this case, **books**, and then we can use \`SET\` followed by the column name we will update. The \`WHERE\` clause specifies which row to update when the clause is met, in this case, the one with **id 1**.
+
+**Delete Operation (DELETE)**
+The **delete** operation removes records from a table. We can achieve this with the \`DELETE\` statement.
+
+**Note**: There is no need to run the query. Deleting this entry will affect the rest of the examples in the upcoming tasks.
+\`\`\`
+mysql> DELETE FROM books WHERE id = 1;
+
+Query OK, 1 row affected (0.00 sec)
+\`\`\`
+
+Above, we can observe the \`DELETE\` statement followed by the \`FROM\` clause, which allows us to specify the table where the record will be removed, in this case, **books**, followed by the \`WHERE\` clause that indicates that it should be the one where the **id** is **1**.
+
+**Summary**
+In summary, **CRUD** operations results are fundamental for data operations and when interacting with databases. The statements associated with them are listed below.
+- **Create (INSERT statement)** - Adds a new record to the table.
+- **Read (SELECT statement)** - Retrieves record from the table.
+- **Update (UPDATE statement)** - Modifies existing data in the table.
+- **Delete (DELETE statement)** - Removes record from the table.
+These operations enable us to effectively manage and manipulate data within a database.
+
+**Answer the questions below**⸻⸻⸻⸻⸻
+
+Using the \`tools_db\` database, what is the name of the tool in the \`hacking_tools\` table that can be used to perform man-in-the-middle attacks on wireless networks?
+**Answer:** Wi-Fi Pineapple
+**Reason:**
+1. Run \`USE tools_db;\`.
+2. Run \`SELECT * FROM hacking_tools;\`.
+
+Using the \`tools_db\` database, what is the shared category for both **USB Rubber Ducky** and **Bash Bunny**?
+**Answer:** USB attacks
+
 
 
 
