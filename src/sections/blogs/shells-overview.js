8f9fa460fdd1c2ac5c9c718b195cec1a5ef8b615
Quang <kylenguyen1206@gmail.com>
2026-02-04 23:03:31 -0500

    update website

---
+++ src/sections/blogs/shells-overview.js
@@ -0,0 +1,41 @@
+export default {
+    id: 'shells-overview',
+    title: 'Shells Overview',
+    category: 'TryHackMe',
+    difficulty: 'Easy',
+    tags: ['Shells'],
+    date: '2026-02-05T12:00:00',
+    excerpt: 'Write up and walkthrough of Shells Overview room on TryHackMe.',
+    content: `
+This is a write up and walkthrough of the Shells Overview room on TryHackMe.
+
+⸻⸻⸻⸻⸻
+
+### Task 1 Introduction
+
+**Introduction**
+Shells in cyber security are widely used by attackers to remotely control systems, making them an important part of the attack chain. In this room, we'll explore different shells used in offensive security, the differences between them, and their use cases. This knowledge can help enhance penetration testing and exploitation skills and also help us understand how to detect when a remote shell is being used by an attacker within an organization.
+
+**Learning Objectives**
+In this room, we'll cover the following learning objectives:
+    - Understand Shells in Offensive Security 
+    - Set Up and Use Reverse and Bind Shells
+    - Deploy Web Shells
+
+**Room Prerequisites**
+An understanding of the following topics is recommended before starting the room:
+    - Basic Understanding of Networking
+    - Fundamental Knowledge of Web Application Security
+    - Basic Command Line Proficiency
+    - Familiarity with scripting languages like Bash, Python, or PHP
+
+**Caveats**
+The use of Metasploit or other Frameworks that generate or interact with shells has been intentionally left behind from this room. This is to focus on understanding how shells work without the use or assistance of a tool to either set up or generate a shell. Also, for this room, we'll use Linux OS for all the examples.
+
+⸻⸻⸻⸻⸻
+
+### Task 2 Shell Overview
+
+
+`
+}
