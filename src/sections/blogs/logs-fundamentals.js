241400ea9217a3ca99faf9eb500f44e0431410f4
Quang <kylenguyen1206@gmail.com>
2026-02-07 22:29:57 -0500

    update website

--- src/sections/blogs/logs-fundamentals.js
+++ src/sections/blogs/logs-fundamentals.js
@@ -146,23 +146,108 @@ You are tasked to find out the activities of the attacker in this compromised sy
 **Answer the questions below**⸻⸻⸻⸻⸻
 
 What is the name of the last user account created on this system?
-**Answer:**
+**Answer:** hacked
+**Reason:**
+1. Open Event Viewer.
+2. Click on 'Windows Logs' then 'Security'.
+3. Click on 'Filter Current Log'.
+4. In the 'Event ID' field, enter 4720 and click 'OK'.
+5. Sort the logs by 'Date and Time' in descending order.
+6. Click on the first log and check the 'New Account' 'Account Name' section.
 
 Which user account created the above account?
-**Answer:**
+**Answer:** Administrator
+1. Open Event Viewer.
+2. Click on 'Windows Logs' then 'Security'.
+3. Click on 'Filter Current Log'.
+4. In the 'Event ID' field, enter 4720 and click 'OK'.
+5. Sort the logs by 'Date and Time' in descending order.
+6. Click on the first log and check the 'Subject' 'Account Name' section.
 
 On what date was this user account enabled? Format: M/D/YYYY
-**Answer:**
+**Answer:** 6/7/2024
+**Reason:**
+1. Open Event Viewer.
+2. Click on 'Windows Logs' then 'Security'.
+3. Click on 'Filter Current Log'.
+4. In the 'Event ID' field, enter 4722 and click 'OK'.
+5. Sort the logs by 'Time Created' in descending order.
+6. Check the first log 'Date and Time' entry.
 
 Did this account undergo a password reset as well? Format: Yes/No
-**Answer:**
+**Answer:** Yes
+**Reason:**
+1. Open Event Viewer.
+2. Click on 'Windows Logs' then 'Security'.
+3. Click on 'Filter Current Log'.
+4. In the 'Event ID' field, enter 4724 and click 'OK'.
+5. Check if there are any logs that contains the account name.
 
 ⸻⸻⸻⸻⸻
 
+### Task 4 Web Server Access Logs Analysis
 
+We interact with many websites daily. Sometimes, we just want to view the website, and sometimes, we want to log in or upload a file into any available input field. These are just different kinds of requests we make to a website. All these requests are logged by the website and stored in a log file on the web server running that website.
 
+This log file contains all the requests made to the website along with the information on the timeframe, the IP requested, the request type, and the URL. Following are the fields taken from a sample log from an Apache web server access log file which can be found in the directory: \`/var/log/apache2/access.log \` 
 
+    - **IP Address:** “172.16.0.1” - The IP address of the user who made the request.
+    - **Timestamp:** “[06/Jun/2024:13:58:44]” - The time when the request was made to the website.
+    - **Request:** The request details.
+        - **HTTP Method:** “GET” - Tells the website what action to be performed on the request.
+        - **URL:** “/” - The requested resource.
+    - **Status Code:** “200” - The response from the server. Different numbers indicate different response results.
+    - **User-Agent:** “Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36” - Information about the user’s Operating System, browser, etc. when making the request.
 
+We can perform manual log analysis by using some command line utilities in the Linux operating system. The following are some commands that can be useful during manual log analysis.
 
+\`cat\` is a popular utility for displaying the contents of a text file. We can use the cat command to display the contents of a log file, as they are typically in the text format.
+\`\`\`
+root@kali$ cat access.log
+172.16.0.1 - - [06/Jun/2024:13:58:44] "GET /products HTTP/1.1" 404 "-" "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36"
+10.0.0.1 - - [06/Jun/2024:13:57:44] "GET / HTTP/1.1" 404 "-" "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36"
+192.168.1.1 - - [06/Jun/2024:13:56:44] "GET /about HTTP/1.1" 500 "-" "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36"
+\`\`\`
+
+Most systems rotate the logs regularly. This rotation helps them create individual log files for specific timeframes and not carry them all in a single log file. But sometimes, we may need to combine two log files. The cat command-line utility can also be helpful in this case. We can combine the results of multiple files within one single file, as shown below.
+\`\`\`
+root@kali$ cat access1.log access2.log > combined_access.log
+\`\`\`
+
+\`grep\` is a very useful command line utility that allows you to search for strings and patterns inside a log file. For example, you may need to search if a specific IP address is present in your log file. You can do this by using the following command: The following command will search the access.log file for the string ‘192.168.1.1’ and display all the lines with this string.
+\`\`\`
+root@kali$ grep "192.168.1.1" access.log
+192.168.1.1 - - [06/Jun/2024:13:56:44] "GET /about HTTP/1.1" 500 "-" "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36"
+192.168.1.1 - - [06/Jun/2024:13:53:44] "GET /products HTTP/1.1" 404 "-" "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36"
+192.168.1.1 - - [06/Jun/2024:13:46:44] "GET /about HTTP/1.1" 200 "-" "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36"
+\`\`\`
+
+The \`less\` command is helpful for handling multiple log files. You may need to analyze specific chunks one by one. For this, you can use the less command-line utility, which helps you view one page at a time.
+\`\`\`
+root@kali$ less access.log
+172.16.0.1 - - [06/Jun/2024:13:52:44] "GET /products HTTP/1.1" 404 "-" "Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36"
+10.0.0.1 - - [06/Jun/2024:13:48:44] "GET /about HTTP/1.1" 404 "-" "Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36"
+192.168.1.1 - - [06/Jun/2024:13:46:44] "GET /about HTTP/1.1" 200 "-" "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36"
+:
+\`\`\`
+
+Use \`spacebar\` to move to the next page and \`b\` to the previous page.
+
+After running this command, the logs would be displayed in pages, one after one, making it easy for manual analysis. If you wish to search for something in the logs, you can type \`\\\` followed by the pattern you are searching for and hit enter.
+
+Use \`n\` to navigate to the next occurrence of your search and \`N\` to navigate to the previous occurrence of your search.
+
+**Answer the questions below**⸻⸻⸻⸻⸻
+
+What is the IP which made the last GET request to URL: “/contact”?
+**Answer:** 10.0.0.1
+**Reason:** Run \`grep "GET /contact" access.log\`.
+
+When was the last POST request made by IP: “172.16.0.1”?
+**Answer:** 06/Jun/2024:13:55:44
+**Reason:** Run \`grep -e "POST" -e "172.16.0.1" access.log\`.
+
+Based on the answer from question number 2, to which URL was the POST request made?
+**Answer:** /contact
 `
 }
