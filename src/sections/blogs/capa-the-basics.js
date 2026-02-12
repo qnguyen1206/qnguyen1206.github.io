7b7fdf9468d16e02b6b3526fdc85ca6880722f43
Quang <kylenguyen1206@gmail.com>
2026-02-12 13:20:52 -0500

    update website

--- src/sections/blogs/capa-the-basics.js
+++ src/sections/blogs/capa-the-basics.js
@@ -589,17 +589,235 @@ To further explain this, let’s check the first capability on the table, “**r
 This tells us that CAPA was able to identify that the potentially malicious software **searches for VMware-specific registry keys**, the **presence of VMware tools**, or other **VM-related elements** by using the **reference-anti-vm-strings.yml** rule yaml file. Malware typically does this behaviour to avoid detection. That is why CAPA flagged this one.
 
 Let’s have another example. Let’s look at "**schedule task via schtasks**".
+- We note that the related rules in YML format is **schedule-task-via-schtasks.yml**
+- This is under the namespace **scheduled-tasks**
+- which is also under the Top-Level Namespace **persistence**
 
+This tells us that CAPA could identify behaviours related to scheduled tasks within the Windows operating system. It might have recognized patterns indicating that the executable registers itself as a scheduled task to maintain persistence using the rule defined in **schedule-task-via-schtasks.yml**.
 
+Hold on! Have you noticed something?
 
+That’s right! The item under Capability has the same name as the YML files under the Rules, with the addition of a dash (-) character between spaces! Simple because Capability is the name of the rule.
 
+Now, we want to take note of some exceptions here. Where the Capability or rules are not located under its Namespace, let’s take the Capability **reference cryptocurrency strings** from the table above; this should be under the **Impact** Top-Level Namespace, right? However, if you go through the folders, you won’t be able to find the corresponding rules. It will be located under the **Nursery** TLN. This is the placeholder for rules that are not quite polished yet.
 
+Now that we have a good overview and understanding of the Capability and Namespace contents, we should be able to explain the sample result from the previous tasks. Therefore, let’s do a quick recap using one of the results. Shall we?
+\`\`\`
+┌───────────────────────────────────────────┬───────────────────────────────────────────┐
+│ Capability                                │ Namespace                                 │
+├───────────────────────────────────────────┼───────────────────────────────────────────┤
+│ reference Base64 string                   │ data-manipulation/encoding/base64         │
+└───────────────────────────────────────────┴───────────────────────────────────────────┘
+\`\`\`
 
+Here's the explanation for the above result. See the table below.
+[table]
+Label | Value | Explanation
+Capability | reference base64 string | Malware has the capability to encode data using a base64 scheme.
+Top-Level Namespace | data-manipulation | contains a set of rules that govern the behaviors involved in altering data within executable files. This aspect can be considered the “data transformation” component of malware behaviour, encompassing actions such as String Encryption and Data Encoding.
+Namespace | encoding/base64 | this namespace consists of rules for encoding and decoding data using Base64 and XOR
+Rule YAML File Matched? | reference-base64-string.yml | Remember that the capability's name is also the rule's name with an additional dash (-) character between spaces.
+[/table]
 
+Knowing this information, you may say this file can use the base64 encoding scheme!
 
+Now we’re done dissecting the results from the sample run from the previous task!
 
+**Answer the questions below**⸻⸻⸻⸻⸻
 
+What **rule yaml file** was matched if the Capability or rule name is **check HTTP status code**?
+**Answer:** check-http-status-code.yml
 
+What is the **name of the Capability** if the rule YAML file is \`reference-anti-vm-strings.yml\`?
+**Answer:**
 
+Which **TLN** or Top-Level Namespace includes the Capability or rule name **run PowerShell expression**?
+**Answer:** load-code
+
+Check the conditions inside the \`check-for-windows-sandbox-via-registry.yml\` rule file from this <a href="https://github.com/MBCProject/capa-rules-1/blob/master/anti-analysis/anti-vm/vm-detection/check-for-windows-sandbox-via-registry.yml">link</a>. What is the **value of the API** that ends in \`Ex\` is it looking for?
+**Answer:** RegOpenKeyEx
+
+⸻⸻⸻⸻⸻
+
+### Task 7 More Information, more fun!
+
+Aren't you curious about what precisely within the rule was matched? Well, I am!
+
+In this task, we will seek to determine the reason for triggering the rules and the conditions involved. We will use the parameter \`-vv\`  or **very verbose** to achieve this.
+[table]
+Option | Description | Sample Syntax
+\`-v\` or \`--verbose\` | enable verbose result document | \`capa.exe -v .\\cryptbot.bin\`
+\`-vv\` or \`--vverbose\` | enable very verbose result document) | \`capa.exe -vv .\\cryptbot.bin\`
+[/table]
+
+Let's run it!
+\`\`\`
+PS C:\\Users\\Administrator\\Desktop\\capa> capa -vv .\\cryptbot.bin
+loading : 100%|████████████████████| 485/485 [00:00<00:00, 1108.84     rules/s]
+/ analyzing program...
+\`\`\`
+
+This will give us more detailed results; however, this will take a lot of time. We already processed this and named the file \`cryptbot_vv.txt\` in "\`C:\\Users\\Administrator\\Desktop\\capa\`". Remember, you don't need to wait for the tool to finish running; we did this so that you can get a feel for running the tool and experiment with its parameters.
+
+Open another PowerShell terminal and look inside the file using the command \`Get-Content cryptbot_vv.txt\`.
+\`\`\`
+PS C:\\Users\\Administrator\\Desktop\\capa> Get-Content .\\cryptbot_vv.txt
+\`\`\`
+
+So, have you opened the file? Do you have more than three thousand lines similar to this one?
+
+Accessing this vast amount of information using a terminal or a text editor will be challenging.
+
+To analyze this result with ease, we need to do two things. First, we will use the parameter \`-j\` and \`-vv\`, and direct the result to a **.json** file. The command would be \`capa.bin -j -vv .\\cryptbot.bin > cryptbot_vv.json\`
+\`\`\`
+PS C:\\Users\\Administrator\\Desktop\\capa> capa.bin -j -vv .\\cryptbot.bin > cryptbot_vv.json
+loading : 100%|████████████████████| 485/485 [00:00<00:00, 1108.84     rules/s]
+/ analyzing program...
+\`\`\`
+
+Again, this will take a lot of time, similar to the previous commands we run. We have processed this and created a file named **cryptbot_vv.json** located in "**C:\\Users\\Administrator\\Desktop\\capa**". Additionally, we've attached the file to this task. Remember, you don't need to wait for the tool to finish running; we did this so that you can get a feel for running the tool and experiment with its parameters. 
+
+We can start with our next step.
+
+**CAPA Web Explorer**
+The second thing we need to do is upload the file to **CAPA Explorer Web**. We can either use their online version here on this <a href="https://mandiant.github.io/capa/explorer/#/">link</a>, or the offline version already in the virtual machine. There is a Google Chrome browser on the desktop named **capa_web_explorer_offline.html**. Alternatively, you can access this using a regular Chrome browser bookmark. Note that the local page may take up to a minute to load in Chrome on the target machine.
+
+Now, we should have access to the homepage!
+<img src="/blogs/tryhackme/capa-the-basics/5e6bbe59a46ee9407fd65bbe-1726485953392.png">
+
+Look for the button **Upload from local** located at the bottom left of the page and select the file **cryptbot_vv.json** in **C:\\Users\\Administrator\\Desktop\\capa**. Once uploaded, you should have a similar output.
+<img src="/blogs/tryhackme/capa-the-basics/5e6bbe59a46ee9407fd65bbe-1726486090871.png">
+
+Doesn't this look good and easier to use? I bet it is!
+
+Now, it's time to explore this excellent addition to the tool. We'll review some capabilities and check what precisely within the rule was matched. This will give us a better idea of how the rule works.
+
+Let's go through our first example below. We know that the capability was to **reference anti-VM strings targeting VMWare**, and the corresponding rule config file or yaml file is **anti-VM-Strings-targeting-VMWare.yml**. Notice the box from the image.
+<img src="/blogs/tryhackme/capa-the-basics/5e6bbe59a46ee9407fd65bbe-1726487492872.png">
+
+Then, let us show you an overview of the rule's content. Focus on the **features** as CAPA uses the succeeding strings below to check if strings are within the analyzed file. 
+\`\`\`
+rule:
+  meta:
+    name: reference anti-VM strings targeting VMWare
+    namespace: anti-analysis/anti-vm/vm-detection
+    authors:
+      - michael.hunhoff@mandiant.com
+      - "@johnk3r"
+    scopes:
+      static: file
+      dynamic: file
+    att&ck:
+      - Defense Evasion::Virtualization/Sandbox Evasion::System Checks [T1497.001]
+    mbc:
+      - Anti-Behavioral Analysis::Virtual Machine Detection [B0009]
+    references:
+      - https://github.com/LordNoteworthy/al-khaser/blob/master/al-khaser/AntiVM/VMWare.cpp
+    examples:
+      - al-khaser_x86.exe_
+      - b83480162ede09d4aa6d4850f9faa0a4c3834152752fd04cfdb22d647aa1f825:0x17D80
+  features:
+    - or:
+      - string: /VMWare/i
+      - string: /VMTools/i
+      - string: /SOFTWARE\\\\VMware, Inc\\.\\\\VMware Tools/i
+      - string: /VMWare/i
+      - string: /VMTools/i
+      - string: /SOFTWARE\\\\VMware, Inc\\.\\\\VMware Tools/i
+      - string: /vmnet\\.sys/i
+      - string: /vmmouse\\.sys/i
+      - string: /vmusb\\.sys/i
+      - string: /vm3dmp\\.sys/i
+      - string: /vmci\\.sys/i
+      - string: /vmhgfs\\.sys/i
+      - string: /vmmemctl\\.sys/i
+      - string: /vmx86\\.sys/i
+      - string: /vmrawdsk\\.sys/i
+      - string: /vmusbmouse\\.sys/i
+      - string: /vmkdb\\.sys/i
+      - string: /vmnetuserif\\.sys/i
+      - string: /vmnetadapter\\.sys/i
+      - string: /\\\\\\\\.\\\\HGFS/i
+      - string: /\\\\\\\\.\\\\vmci/i
+      - string: /vmtoolsd\\.exe/i
+      - string: /vmwaretray\\.exe/i
+      - string: /vmwareuser\\.exe/i
+      - string: /VGAuthService\\.exe/i
+      - string: /vmacthlp\\.exe/i
+      - string: /vmci/i
+        description: VMWare VMCI Bus Driver
+      - string: /vmhgfs/i
+        description: VMWare Host Guest Control Redirector
+      - string: /vmmouse/i
+      - string: /vmmemctl/i
+        description: VMWare Guest Memory Controller Driver
+      - string: /vmusb/i
+      - string: /vmusbmouse/i
+      - string: /vmx_svga/i
+      - string: /vmxnet/i
+      - string: /vmx86/i
+      - string: /VMwareVMware/i
+      - string: /vmGuestLib\\.dll/i
+      - string: /vmGuestLib\\.dll/i
+      - string: /Applications\\\\VMwareHostOpen\\.exe/i
+      - string: /vm3dgl\\.dll/i
+      - string: /vmdum\\.dll/i
+      - string: /vm3dver\\.dll/i
+      - string: /vmtray\\.dll/i
+      - string: /VMToolsHook\\.dll/i
+      - string: /vmmousever\\.dll/i
+      - string: /VmGuestLibJava\\.dll/i
+      - string: /vmscsi\\.sys/i
+\`\`\`
+
+Did you see it? That's right! Under the features, the "**string: /VMWare/i**" is being referenced by CAPA Web Explorer. Simply, CAPA is saying that under this namespace, we could identify strings with a value of **VMWare** by using the conditions within the rule and with regex.
+
+Let's have another sample.  We know that the capability was to reference the **scheduled task via schtasks**, and the corresponding rule was to schedule the task via **schtasks.yml**. Notice the box from the image.
+<img src="/blogs/tryhackme/capa-the-basics/5e6bbe59a46ee9407fd65bbe-1726488911179.png">
+
+The same applies to our first example; we will show you the rule's content overview. Focus on the features, as CAPA uses the succeeding strings below to check if there are strings within the file being analyzed.
+\`\`\`
+rule:
+  meta:
+    name: schedule task via schtasks
+    namespace: persistence/scheduled-tasks
+    authors:
+      - 0x534a@mailbox.org
+    scopes:
+      static: function
+      dynamic: thread
+    att&ck:
+      - Persistence::Scheduled Task/Job::Scheduled Task [T1053.005]
+    examples:
+      - 79cde1aa711e321b4939805d27e160be:0x401440
+  features:
+    - and:
+      - match: host-interaction/process/create
+      - or:
+        - and:
+          - string: /schtasks/i
+          - string: /\\/create /i
+        - string: /Register-ScheduledTask /i
+\`\`\`
+
+Under the feature, the "**string: /schtasks/i and /\\/create /i**" is referenced by CAPA Web Explorer. Simply, CAPA is saying under this namespace, and by using the conditions within the rule and with regex, we could identify strings with a value of **schtasks** and **create**. 
+
+**Global Search Box**
+Another cool feature of this tool is its filter options and the Global Search box, which are very helpful.
+
+We could quickly examine this overwhelming information using CAPA Web Explorer compared to any text editor. 
+
+Feel free to explore more! 
+
+**Answer the questions below**⸻⸻⸻⸻⸻
+
+Which parameter allows you to output the result of CAPA into a **.json file**?
+**Answer:** -j
+
+What tool allows you to interactively explore CAPA results in your web browser?
+**Answer:** CAPA Web Explorer
+
+Which feature of this CAPA Web Explorer allows you to filter options or results?
+**Answer:** Global Search Box
 `
 }
