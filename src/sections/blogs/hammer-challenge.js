f0a4df9affb01f9cd40bb8bc6c1b9a2aca48f978
Quang <kylenguyen1206@gmail.com>
2026-04-14 20:51:30 -0400

    update website

--- src/sections/blogs/hammer-challenge.js
+++ src/sections/blogs/hammer-challenge.js
@@ -21,7 +21,14 @@ What is the flag value after logging in to the dashboard?
 **Answer:** THM{AuthBypass3D}
 **Reason:**
 1. Run \`nmap -sV -sC -p1-65535 MACHINE\_IP\` to figure out if the host is online and which port is open. We will find that port 22 for SSH and port 1337 for HTTP are open.
-2. Go to the website at \`http://MACHINE_IP:1337\` and we will see a login page.
+2. Go to the website at \`http://MACHINE\_IP:1337\` and we will see a login page.
+3. After inspect the page, we will find that there is a dev note that said "<!-- Dev Note: Directory naming convention must be hmr_DIRECTORY_NAME -->".
+4. We can use this information to brute force the directories using \`dirb\` or \`gobuster\`.
+5. Before we brute force the directories, we need to create a wordlist that contains the directory naming convention. We can use \`sed\` to modify the wordlist from \`/usr/share/wordlists/dirb/big.txt\` in order to prepend \`hmr_\` in front of each word. The command will be \`sed 's/^/hmr_/' /usr/share/wordlists/dirb/big.txt > hmr_big.txt\`.
+6. Now, we can run \`dirb http://MACHINE\_IP:1337 hmr_big.txt\`.
+7. We will find some directories including \`hmr_css\`, \`hmr_images\`, \`hmr_js\`. However, we are interested in the \`hmr_logs\` since it might give us something useful.
+8. After accessing \`http://MACHINE\_IP:1337/hmr_logs\`, we will find a log file that contains some errors in which one of the username \`tester@hammer.thm\` was exposed.
+9.
 
 
 What is the content of the file **/home/ubuntu/flag.txt**?
--- src/sections/certificates.js
+++ src/sections/certificates.js
@@ -2,15 +2,13 @@ const certificatesData = {
   professional: [
     
   ],
-  pce: [
+  nonProfessional: [
     {
       title: "IT Security Foundations: Network Security",
       issuer: "NASBA",
       date: "Apr 9, 2026",
       link: "https://www.linkedin.com/learning/certificates/1eb6a17bed666fffc16b4fa913271d42d186094d0925bbbd294cb341d03edb7c?trk=share_certificate"
-    }
-  ],
-  nonProfessional: [
+    },
     {
       title: "IT Security Foundations: Network Security",
       issuer: "LinkedIn Learning",
@@ -229,16 +227,6 @@ export function initCertificates() {
             <path d="M3 13a20 20 0 0 0 18 0" />
           </svg>
           Professional
-        </button>
-        <button class="cert-tab-btn" data-tab="pce">
-          <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
-            <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
-            <path d="M3 7m0 2a2 2 0 0 1 2 -2h14a2 2 0 0 1 2 2v9a2 2 0 0 1 -2 2h-14a2 2 0 0 1 -2 -2z" />
-            <path d="M8 7v-2a2 2 0 0 1 2 -2h4a2 2 0 0 1 2 2v2" />
-            <path d="M12 12l0 .01" />
-            <path d="M3 13a20 20 0 0 0 18 0" />
-          </svg>
-          PCE
         </button>
         <button class="cert-tab-btn" data-tab="nonProfessional">
           <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
