1079b03aefb9b04b4a12359afbf54c9bb5afd347
Quang <kylenguyen1206@gmail.com>
2026-04-16 20:38:37 -0400

    update website

--- src/sections/blogs/pickle-rick-challenge.js
+++ src/sections/blogs/pickle-rick-challenge.js
@@ -13,7 +13,7 @@ This is the write up and walkthrough of the Pickle Rick Challenge room on TryHac
 
 This Rick and Morty-themed challenge requires you to exploit a web server and find three ingredients to help Rick make his potion and transform himself back into a human from a pickle.
 
-Deploy the virtual machine on this task and explore the web application: MACHINE\_IP
+Deploy the virtual machine on this task and explore the web application: MACHINE\\_IP
 
 **Answer the questions below**⸻⸻⸻⸻⸻
 
