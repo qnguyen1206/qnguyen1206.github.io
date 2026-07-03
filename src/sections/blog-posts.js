031441e5916d1e157162d7bda1d4d6d99fd5880c
Quang <kylenguyen1206@gmail.com>
2026-07-02 20:31:46 -0400

    update website

--- src/sections/blog-posts.js
+++ src/sections/blog-posts.js
@@ -286,7 +286,7 @@ export const blogPosts = [
     windowWashingGameMDM,
     //mythicalHuntMDM,
     //lmc4720InteractiveNarrativeProject,
-    kartTheTechFilledRacingGame,
+    //kartTheTechFilledRacingGame,
 
     /* Design */
     biophiliaDesignProcess,
